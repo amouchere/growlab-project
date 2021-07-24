ae5e06901f5109cd50d00586668d7d7200242b70
AMouchere <a.mouchere@protonmail.com>
2021-07-24 20:34:43 +0100

    :sparkles: add pressure sensor

--- sensors.py
+++ sensors.py
@@ -1,8 +1,16 @@
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+from bme280 import BME280
+from bmp280 import BMP280
 import Adafruit_DHT
 import time
 import pytz
 import serial
 from datetime import datetime
+
+
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
@@ -12,6 +20,10 @@ DHT_PIN = 4
 serialArduino = serial.Serial('/dev/ttyUSB0', 9600)
 
 class sensors:
+
+    def __init__(self):
+        self.bus = SMBus(1)
+        self.sensor = BMP280(i2c_dev=self.bus)
 
     def get_readings(self):
         # DHT22
@@ -31,9 +43,20 @@ class sensors:
         lux = serialOutputArray[1].replace('lux:', '')
         soilHumiditypercent = serialOutputArray[2].replace('humpercent:', '')
 
+        # BMP280
+         # Ignore first result since it seems stale
+        temperatureBMP280 = self.sensor.get_temperature()
+        pressure = self.sensor.get_pressure()
+        time.sleep(0.1)
+
+        temperatureBMP280 = self.sensor.get_temperature()
+        pressure = self.sensor.get_pressure()
+
         return {
             "time": time_str,
             "temperature": temperature,
+            "temperatureBMP280": temperatureBMP280,
+            "pressure": pressure,
             "humidity": humidity,
             "brightness": lux,
             "soilHumidity": soilHumidity,
