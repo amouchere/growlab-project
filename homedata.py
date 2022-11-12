6e001e5e5ecff5f12dbbc0ac378bff035cfc7c3f
Antoine Mouchère <a.mouchere@gmail.com>
2022-11-12 21:48:51 +0100

    :sparkles: add a configuration to disable statistics sending

--- homedata.py
+++ homedata.py
@@ -5,25 +5,27 @@ class homedata:
         self.config = config
 
     def send(self, readings):
-        logger = logging.getLogger("growlab")
+        statIsActive = self.config["isActive"]
+        if statIsActive == "true" :
+            logger = logging.getLogger("growlab")
 
-        data = {"location":"growlab","table":[]}
-        pairs = readings.items()
-        for key, value in pairs:
-            if key != "time":
-                data["table"].append({"key" :key, "value": value})
-        
-        
-        logger.info("request: {} {}".format(self.config["url"], data))
+            data = {"location":"growlab","table":[]}
+            pairs = readings.items()
+            for key, value in pairs:
+                if key != "time":
+                    data["table"].append({"key" :key, "value": value})
+            
+            
+            logger.info("request: {} {}".format(self.config["url"], data))
 
-        try:
-            x = requests.post(self.config["url"], json.dumps(data))
-        except Exception as e:
-            logging.error("Error: {}".format(e))
+            try:
+                x = requests.post(self.config["url"], json.dumps(data))
+            except Exception as e:
+                logging.error("Error: {}".format(e))
 
-        
+            
 
-        #print the response text (the content of the requested file):
-        logger.info("response {}".format(x.text))
+            #print the response text (the content of the requested file):
+            logger.info("response {}".format(x.text))
 
 
